dc1e35ebad92d6d188223e41e529914e33b260c1
heqins <39974148+heqins@users.noreply.github.com>
2019-10-29 22:21:31 +0800

    10/29/19

--- src/pow_x_n.java
+++ src/pow_x_n.java
@@ -2,6 +2,11 @@ public class pow_x_n {
     public static double myPow(double x, int n) {
         if(n==0) return 1;
         if(n<0){
+            // -2147483648 is Integer.MIN_VALUE, and Integer.MAX_VALUE is 2147483647
+            // so n = -n have no sense when n = -2147483648
+            if (n == Integer.MIN_VALUE) {
+                n += 2;
+            }
             n = -n;
             x = 1/x;
         }
@@ -9,6 +14,7 @@ public class pow_x_n {
     }
 
     public static void main(String[] args) {
-        System.out.println(myPow(2, 3));
+        System.out.println(myPow(-1.0, -2147483648));
+        System.out.println(Math.pow(-1.0, Integer.MIN_VALUE + 2));
     }
 }
