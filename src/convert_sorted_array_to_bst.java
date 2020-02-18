28d0146dc6ec0595c58673a005e8e2390b61eb9b
heqins <39974148+heqins@users.noreply.github.com>
2020-02-18 13:02:53 +0800

    Update convert_sorted_array_to_bst.java

--- src/convert_sorted_array_to_bst.java
+++ src/convert_sorted_array_to_bst.java
@@ -7,6 +7,7 @@ public class convert_sorted_array_to_bt {
         if (start > end) {
             return null;
         }
+        // 二叉搜索树根节点从中间开始
         int mid = (start + end) / 2;
         TreeNode root = new TreeNode(nums[mid]);
         root.left = helper(nums, start, mid-1);
