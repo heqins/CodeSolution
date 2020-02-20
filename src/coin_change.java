fa880ad4bcb745dfe8c9db4ed9c46c0caf6d0dbc
heqins <39974148+heqins@users.noreply.github.com>
2020-02-20 17:26:08 +0800

    Update coin_change.java

--- src/coin_change.java
+++ src/coin_change.java
@@ -1,3 +1,19 @@
+/**
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+
+Input: coins = [1, 2, 5], amount = 11
+Output: 3 
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Note:
+You may assume that you have an infinite number of each kind of coin.
+*//
+
 public class coin_change {
     /**
      * 我们维护一个一维动态数组 dp，其中 dp[i] 表示钱数为i时的最小硬币数的找零，注意由于数组是从0开始的，
@@ -24,6 +40,7 @@ public class coin_change {
         int[] dp = new int[amount + 1];
 
         for (int i=1; i < dp.length; i++) {
+            //初始化值注意
             dp[i] = dp.length;
             for (int j=0; j < coins.length; j++) {
                 if (i >= coins[j]) dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
