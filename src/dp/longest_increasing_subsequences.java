b43967ea6fc86afdac7f16f3e9d654ae87bd337d
heqins <39974148+heqins@users.noreply.github.com>
2020-02-20 16:09:31 +0800

    Update and rename src/longest_increasing_subsequences.java to src/dp/longest_increasing_subsequences.java

--- src/dp/longest_increasing_subsequences.java
+++ src/dp/longest_increasing_subsequences.java
@@ -1,3 +1,21 @@
+/**
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+
+Input: [10,9,2,5,3,7,101,18]
+Output: 4 
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+
+Note:
+
+    There may be more than one LIS combination, it is only necessary for you to return the length.
+    Your algorithm should run in O(n2) complexity.
+
+Follow up: Could you improve it to O(n log n) time complexity?
+
+**/
+
 public class longest_increasing_subsequences {
     public static int lengthOfLIS(int[] nums) {
         int[] tails = new int[nums.length];
