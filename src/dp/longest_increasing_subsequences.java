8d50d561c1a00aa2bf0c53b77b9f8fb716e202bb
heqins <39974148+heqins@users.noreply.github.com>
2020-02-20 16:40:32 +0800

    Update longest_increasing_subsequences.java

--- src/dp/longest_increasing_subsequences.java
+++ src/dp/longest_increasing_subsequences.java
@@ -17,6 +17,13 @@ Follow up: Could you improve it to O(n log n) time complexity?
 **/
 
 public class longest_increasing_subsequences {
+    /**
+    首先来看一种动态规划 Dynamic Programming 的解法，这种解法的时间复杂度为 O(n2)，
+    类似 brute force 的解法，维护一个一维 dp 数组，其中 dp[i] 表示以 nums[i] 为结尾的最长递增子串的长度，
+    对于每一个 nums[i]，从第一个数再搜索到i，如果发现某个数小于 nums[i]，更新 dp[i]，更新方法为 dp[i] = max(dp[i], dp[j] + 1)，
+    即比较当前 dp[i] 的值和那个小于 num[i] 的数的 dp 值加1的大小，就这样不断的更新 dp 数组，
+    到最后 dp 数组中最大的值就是我们要返回的 LIS 的长度
+    **/
     public static int lengthOfLIS(int[] nums) {
         if (nums.length == 0) {
             return 0;
