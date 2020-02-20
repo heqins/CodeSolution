ef8937a18a80322651cfbf655cc8b97f0fef16cc
heqins <39974148+heqins@users.noreply.github.com>
2020-02-20 16:39:46 +0800

    Update longest_increasing_subsequences.java

--- src/dp/longest_increasing_subsequences.java
+++ src/dp/longest_increasing_subsequences.java
@@ -18,21 +18,23 @@ Follow up: Could you improve it to O(n log n) time complexity?
 
 public class longest_increasing_subsequences {
     public static int lengthOfLIS(int[] nums) {
-        int[] tails = new int[nums.length];
-        int size = 0;
-        for (int x : nums) {
-            int i = 0, j = size;
-            while (i != j) {
-                int m = (i + j) / 2;
-                if (tails[m] < x)
-                    i = m + 1;
-                else
-                    j = m;
-            }
-            tails[i] = x;
-            if (i == size) ++size;
+        if (nums.length == 0) {
+            return 0;
         }
-        return size;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
     }
 
     public static void main(String[] args) {
