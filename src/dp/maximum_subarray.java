01e1179a537bcd9a10045953eaeddf11283e5315
heqins <39974148+heqins@users.noreply.github.com>
2020-02-23 20:46:49 +0800

    Update and rename src/maximum_subarray.java to src/dp/maximum_subarray.java

--- src/dp/maximum_subarray.java
+++ src/dp/maximum_subarray.java
@@ -1,7 +1,15 @@
 /**
- * Given an integer array nums, find the contiguous subarray (containing at least one number)
- * which has the largest sum and return its sum.
- */
+ Given an integer array nums, find the contiguous subarray (containing at least one number) 
+ 
+ which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+ **/
+
 public class maximum_subarray {
     public int maxSubArray2(int[] nums) {
         // bruce force
@@ -21,7 +29,9 @@ public class maximum_subarray {
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
+            // maxSum可以看作是当前连续数组的和，每次都决定是否将当前数加入数组
             maxSum = Math.max(maxSum + nums[i], nums[i]);
+            // dp存的是全局最大的
             dp[i] = Math.max(dp[i-1], maxSum);
         }
         return dp[nums.length - 1];
