8286127ce2ca0604efa204326d8cf815b5317c7b
heqins <39974148+heqins@users.noreply.github.com>
2020-03-09 20:39:43 +0800

    Update gas_station.java

--- src/greedy/gas_station.java
+++ src/greedy/gas_station.java
@@ -40,20 +40,26 @@ Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
 Travel to station 0. Your tank = 4 - 3 + 2 = 3
 Travel to station 1. Your tank = 3 - 3 + 3 = 3
 You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
-Therefore, you can't travel around the circuit once no matter where you start.
+Therefore, you can't travel around the circuit once no matter where you start
+
+这道转圈加油问题不算很难，只要想通其中的原理就很简单。
+我们首先要知道能走完整个环的前提是gas的总量要大于cost的总量，这样才会有起点的存在。
+假设开始设置起点start = 0, 并从这里出发，如果当前的gas值大于cost值，就可以继续前进，此时到下一个站点，
+剩余的gas加上当前的gas再减去cost，看是否大于0，若大于0，则继续前进。当到达某一站点时，若这个值小于0了，
+则说明从起点到这个点中间的任何一个点都不能作为起点，则把起点设为下一个点，继续遍历。当遍历完整个环时，当前保存的起点即为所求。代码如下：
 **/
 
 public class gas_station {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // All we need to do is to find the lowest point as our starting line.
-        int sum = 0, minGas = 0, index = 0;
-        for (int i=0; i<gas.length; ++i) {
-            sum += gas[i]-cost[i];
-            if (sum<minGas) {
-                minGas = sum;
-                index = i+1;
+       int total = 0, sum = 0, start = 0;
+       for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                start = i + 1;
+                sum = 0;
             }
-        }
-        return sum>=0?(index==gas.length?0:index):-1;
+       }
+       return (total < 0) ? -1 : start;
     }
 }
