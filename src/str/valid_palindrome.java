eeecaf7301b5df47c1f6b74cc824ad550f05b005
heqins <39974148+heqins@users.noreply.github.com>
2020-04-01 20:57:51 +0800

    Update and rename src/valid_palindrome.java to src/str/valid_palindrome.java

--- src/str/valid_palindrome.java
+++ src/str/valid_palindrome.java
@@ -1,3 +1,17 @@
+/**
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: fals
+**/
 public class valid_palindrome {
     public static boolean isPalindrome(String s) {
         if (s.length() == 0) return true;
