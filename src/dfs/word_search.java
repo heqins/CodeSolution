93bae7357dc42be6c60e13406c02a7d9e389949d
heqins <39974148+heqins@users.noreply.github.com>
2020-04-01 20:33:25 +0800

    Update and rename src/word_search.java to src/dfs/word_search.java

--- src/dfs/word_search.java
+++ src/dfs/word_search.java
@@ -1,3 +1,30 @@
+/**
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+ 
+
+Constraints:
+
+board and word consists only of lowercase and uppercase English letters.
+1 <= board.length <= 200
+1 <= board[i].length <= 200
+1 <= word.length <= 10^3
+**/
+
 public class word_search {
     public static boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
