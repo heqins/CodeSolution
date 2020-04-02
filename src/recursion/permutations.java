122f5a9e5ff3cdbe280d3dea595daa9ec2914ce6
heqins <39974148+heqins@users.noreply.github.com>
2020-04-02 20:54:30 +0800

    Update and rename src/permutations.java to src/recursion/permutations.java

--- src/recursion/permutations.java
+++ src/recursion/permutations.java
@@ -1,3 +1,22 @@
+/**
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+permutation-排列
+**/
+
 import java.util.ArrayList;
 import java.util.List;
 
