c8ec12fe1731b709fed89a8e9d15499edc908d0a
heqins <39974148+heqins@users.noreply.github.com>
2020-04-06 17:42:16 +0800

    Update find_first_and_last_position_of_element_in_sorted_array.java

--- src/find_first_and_last_position_of_element_in_sorted_array.java
+++ src/find_first_and_last_position_of_element_in_sorted_array.java
@@ -1,3 +1,20 @@
+/**
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+**/
+
 public class find_first_and_last_position_of_element_in_sorted_array {
     public int[] searchRange(int[] nums, int target) {
         int left = 0;
