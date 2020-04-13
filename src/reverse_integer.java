1c6a1ce1e324041d81873c97c08e9a86ff464bd0
heqins <39974148+heqins@users.noreply.github.com>
2020-04-13 16:39:48 +0800

    Update reverse_integer.java

--- src/reverse_integer.java
+++ src/reverse_integer.java
@@ -30,6 +30,7 @@ public class reverse_integer {
     public int reverse(int x) {
         int y=0;
         for(;x!=0;x/=10){
+            // 注意要/10
             if (y>Integer.MAX_VALUE/10 || y<Integer.MIN_VALUE/10) return 0;
             y= 10*y + x%10;
         }
