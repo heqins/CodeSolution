1bb89512036badc1f7843c16b15ffa5148663e63
heqins <39974148+heqins@users.noreply.github.com>
2020-04-15 12:15:29 +0800

    Update jump_game.java

--- src/greedy/jump_game.java
+++ src/greedy/jump_game.java
@@ -19,6 +19,16 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum
 **/
 public class jump_game {
     // greedy algorithm
+    /**
+    其实这题最好的解法不是 DP，而是贪婪算法 Greedy Algorithm，因为这里并不是很关心每一个位置上的剩余步数，
+    
+    而只希望知道能否到达末尾，也就是说我们只对最远能到达的位置感兴趣，所以维护一个变量 reach，表示最远能到达的位置，
+    
+    初始化为0。遍历数组中每一个数字，如果当前坐标大于 reach 或者 reach 已经抵达最后一个位置则跳出循环，
+    
+    否则就更新 reach 的值为其和 i + nums[i] 中的较大值，其中 i + nums[i] 表示当前位置能到达的最大位置，参见代码如下：
+    **/
+  
     public boolean canJump(int[] nums) {
         int reachable = 0;
         for (int i = 0; i < nums.length; i++) {
