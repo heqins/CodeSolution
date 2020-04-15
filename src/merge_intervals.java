1b5de3ffe8ee1f278b8b8254e72ee24bba5bc776
heqins <39974148+heqins@users.noreply.github.com>
2020-04-15 18:08:56 +0800

    Update merge_intervals.java

--- src/merge_intervals.java
+++ src/merge_intervals.java
@@ -14,6 +14,18 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
 **/
 
+/**
+这道和之前那道 Insert Interval 很类似，这次题目要求我们合并区间，之前那题明确了输入区间集是有序的，而这题没有，
+
+所以我们首先要做的就是给区间集排序，由于我们要排序的是个结构体，所以我们要定义自己的 comparator，才能用 sort 来排序，
+
+我们以 start 的值从小到大来排序，排完序我们就可以开始合并了，首先把第一个区间存入结果中，然后从第二个开始遍历区间集，
+
+如果结果中最后一个区间和遍历的当前区间无重叠，直接将当前区间存入结果中，如果有重叠，
+
+将结果中最后一个区间的 end 值更新为结果中最后一个区间的 end 和当前 end 值之中的较大值，然后继续遍历区间集，以此类推可以得到最终结果，代码如下：
+
+**/
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
