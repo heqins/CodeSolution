367d7f058d6412e96b52def8140faea98009f3c1
heqins <39974148+heqins@users.noreply.github.com>
2020-04-22 14:20:12 +0800

    Update and rename src/binary_tree_zigzag_level_order_traversal.java to src/tree/binary_tree_zigzag_level_order_traversal.java

--- src/tree/binary_tree_zigzag_level_order_traversal.java
+++ src/tree/binary_tree_zigzag_level_order_traversal.java
@@ -1,3 +1,30 @@
+/**
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+**/
+
+/**
+这道二叉树的之字形层序遍历是之前那道 Binary Tree Level Order Traversal 的变形，不同之处在于一行是从左到右遍历，
+
+下一行是从右往左遍历，交叉往返的之字形的层序遍历。最简单直接的方法就是利用层序遍历，并使用一个变量 cnt 来统计当前的层数（从0开始），
+
+将所有的奇数层的结点值进行翻转一下即可，参见代码如下：
+
+**/
+
 import java.util.*;
 
 public class binary_tree_zigzag_level_order_traversal {
