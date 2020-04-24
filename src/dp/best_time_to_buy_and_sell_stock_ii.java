12b65f55c1d64fe144b93dcadd8fc3434dbae88f
heqins <39974148+heqins@users.noreply.github.com>
2020-04-24 17:18:30 +0800

    Update best_time_to_buy_and_sell_stock_ii.java

--- src/dp/best_time_to_buy_and_sell_stock_ii.java
+++ src/dp/best_time_to_buy_and_sell_stock_ii.java
@@ -25,6 +25,15 @@ Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 **/
 
+/**
+这道跟之前那道Best Time to Buy and Sell Stock 买卖股票的最佳时间很类似，但都比较容易解答。这道题由于可以无限次买入和卖出。
+
+我们都知道炒股想挣钱当然是低价买入高价抛出，那么这里我们只需要从第二天开始，如果当前价格比之前价格高，则把差值加入利润中，
+
+因为我们可以昨天买入，今日卖出，若明日价更高的话，还可以今日买入，明日再抛出。以此类推，遍历完整个数组后即可求得最大利润。代码如下：
+
+**/
+
 class Solution {
     // [a, b, c, d, e, f]
     // a < b < c < d > e < f
