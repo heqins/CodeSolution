08527418712dc67a672852839f1114518d649a2a
heqins <39974148+heqins@users.noreply.github.com>
2020-04-28 15:51:43 +0800

    Create reorder_list.java

---
+++ src/linked_list/reorder_list.java
@@ -0,0 +1,60 @@
+/**
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+Example 1:
+
+Given 1->2->3->4, reorder it to 1->4->2->3.
+Example 2:
+
+Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+**/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+ /**
+ 这道链表重排序问题可以拆分为以下三个小问题：
+
+1. 使用快慢指针来找到链表的中点，并将链表从中点处断开，形成两个独立的链表。
+
+2. 将第二个链翻转。
+
+3. 将第二个链表的元素间隔地插入第一个链表中。
+**/
+
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) return;
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        ListNode last = mid, pre = null;
+        while (last != null) {
+            ListNode next = last.next;
+            last.next = pre;
+            pre = last;
+            last = next;
+        }
+        
+        while (head != null && pre != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = pre.next;
+            head.next.next = next;
+            head = next;
+        }
+    }
+}
