c497f940e520a8b760cc9b8b36f02854eb738546
heqins <39974148+heqins@users.noreply.github.com>
2020-06-23 12:28:00 +0800

    Update topK.java

--- src/num/topK.java
+++ src/num/topK.java
@@ -90,6 +90,67 @@ class Solution {
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
+三、二叉搜索树也可以 O(NlogK)O(NlogK)O(NlogK)解决 TopK 问题哦
+
+BST 相对于前两种方法没那么常见，但是也很简单，和大根堆的思路差不多～
+要提的是，与前两种方法相比，BST 有一个好处是求得的前K大的数字是有序的。
+
+因为有重复的数字，所以用的是 TreeMap 而不是 TreeSet（有的语言的标准库自带 TreeMultiset，也是可以的）。
+
+TreeMap的key 是数字，value 是该数字的个数。
+我们遍历数组中的数字，维护一个数字总个数为 K 的 TreeMap：
+1.若目前 map 中数字个数小于 K，则将 map 中当前数字对应的个数 +1；
+2.否则，判断当前数字与 map 中最大的数字的大小关系：若当前数字大于等于 map 中的最大数字，就直接跳过该数字；若当前数字小于 map 中的最大数字，则将 map 中当前数字对应的个数 +1，并将 map 中最大数字对应的个数减 1。
+
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // TreeMap的key是数字, value是该数字的个数。
+        // cnt表示当前map总共存了多少个数字。
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int cnt = 0;
+        for (int num: arr) {
+            // 1. 遍历数组，若当前map中的数字个数小于k，则map中当前数字对应个数+1
+            if (cnt < k) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                cnt++;
+                continue;
+            } 
+            // 2. 否则，取出map中最大的Key（即最大的数字), 判断当前数字与map中最大数字的大小关系：
+            //    若当前数字比map中最大的数字还大，就直接忽略；
+            //    若当前数字比map中最大的数字小，则将当前数字加入map中，并将map中的最大数字的个数-1。
+            Map.Entry<Integer, Integer> entry = map.lastEntry();
+            if (entry.getKey() > num) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                if (entry.getValue() == 1) {
+                    map.pollLastEntry();
+                } else {
+                    map.put(entry.getKey(), entry.getValue() - 1);
+                }
+            }
+            
+        }
+
+        // 最后返回map中的元素
+        int[] res = new int[k];
+        int idx = 0;
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            int freq = entry.getValue();
+            while (freq-- > 0) {
+                res[idx++] = entry.getKey();
+            }
+        }
+        return res;
+    }
+}
+
+
+作者：sweetiee
+链接：https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/3chong-jie-fa-miao-sha-topkkuai-pai-dui-er-cha-sou/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 //  四、数据范围有限时直接计数排序就行了：O(N)O(N)O(N)
 class Solution {
