68a19f29ec461b81bd9783e1f9d374df72e8398a
heqins <39974148+heqins@users.noreply.github.com>
2020-06-29 13:26:33 +0800

    Update majority_element.java

--- src/num/majority_element.java
+++ src/num/majority_element.java
@@ -10,6 +10,15 @@
 作者：jyd
 链接：https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/solution/mian-shi-ti-39-shu-zu-zhong-chu-xian-ci-shu-chao-3/
 来源：力扣（LeetCode）
+
+摩尔投票法：
+
+    票数和： 由于众数出现的次数超过数组长度的一半；若记 众数 的票数为 +1+1+1 ，非众数 的票数为 −1-1−1 ，则一定有所有数字的 票数和 >0> 0>0 。
+    票数正负抵消： 设数组 nums 中的众数为 xxx ，数组长度为 nnn 。若 nums 的前 aaa 个数字的 票数和 =0= 0=0 ，则 数组后 (n−a)(n-a)(n−a) 个数字的 票数和一定仍 >0>0>0 （即后 (n−a)(n-a)(n−a) 个数字的 众数仍为 xxx ）。
+
+作者：jyd
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/solution/mian-shi-ti-39-shu-zu-zhong-chu-xian-ci-shu-chao-3/
+来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 **/
 
