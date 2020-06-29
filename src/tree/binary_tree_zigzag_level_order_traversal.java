ccf1246f5a08bb0d2ae6086aa7ca756c849dc941
heqins <39974148+heqins@users.noreply.github.com>
2020-06-29 11:36:10 +0800

    Update binary_tree_zigzag_level_order_traversal.java

--- src/tree/binary_tree_zigzag_level_order_traversal.java
+++ src/tree/binary_tree_zigzag_level_order_traversal.java
@@ -63,68 +63,42 @@ class Solution {
 }
 
 
+/**
+复杂度分析：
 
-import java.util.*;
+    时间复杂度 O(N)O(N)O(N) ： NNN 为二叉树的节点数量，即 BFS 需循环 NNN 次，占用 O(N)O(N)O(N) ；双端队列的队首和队尾的添加和删除操作的时间复杂度均为 O(1)O(1)O(1) 。
+    空间复杂度 O(N)O(N)O(N) ： 最差情况下，即当树为满二叉树时，最多有 N/2N/2N/2 个树节点 同时 在 deque 中，使用 O(N)O(N)O(N) 大小的额外空间。
 
-public class binary_tree_zigzag_level_order_traversal {
-    private Stack<TreeNode> leftStack = new Stack();
-    private Stack<TreeNode> rightStack = new Stack();
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        leftStack.push(root);
+打印： 若为奇数层，将 node.val 添加至 tmp 尾部；否则，添加至 tmp 头部；
 
-        List<List<Integer>> returnList = new ArrayList();
-        if(root == null) {
-            return returnList;
+代码：
+
+    Python 中使用 collections 中的双端队列 deque() ，其 popleft() 方法可达到 O(1)O(1)O(1) 时间复杂度；列表 list 的 pop(0) 方法时间复杂度为 O(N)O(N)O(N) 。
+    Java 中将链表 LinkedList 作为双端队列使用。
+**/
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                if(res.size() % 2 == 0) tmp.addLast(node.val); // 偶数层 -> 队列头部
+                else tmp.addFirst(node.val); // 奇数层 -> 队列尾部
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
         }
-
-        List<Integer> fList = new ArrayList();
-        fList.add(root.val);
-        returnList.add(fList);
-
-        while(!leftStack.isEmpty() || !rightStack.isEmpty()) {
-
-            List<Integer> lList = new ArrayList();
-            while(!leftStack.isEmpty()) {
-                TreeNode lNode = leftStack.pop();
-
-
-                if(lNode.right != null) {
-                    rightStack.push(lNode.right);
-                    lList.add(lNode.right.val);
-                }
-
-                if(lNode.left != null) {
-                    rightStack.push(lNode.left);
-                    lList.add(lNode.left.val);
-                }
-            }
-
-            if(!lList.isEmpty()) {
-                returnList.add(lList);
-            }
-
-            List<Integer> rList = new ArrayList();
-            while(!rightStack.isEmpty()) {
-                TreeNode rNode = rightStack.pop();
-
-
-                if(rNode.left != null) {
-                    leftStack.push(rNode.left);
-                    rList.add(rNode.left.val);
-                }
-
-                if(rNode.right != null) {
-                    leftStack.push(rNode.right);
-                    rList.add(rNode.right.val);
-                }
-            }
-
-            if(!rList.isEmpty()) {
-                returnList.add(rList);
-            }
-        }
-
-        return returnList;
+        return res;
     }
 }
+
+作者：jyd
+链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/solution/mian-shi-ti-32-iii-cong-shang-dao-xia-da-yin-er--3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
