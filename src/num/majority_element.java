639ed9bdff6961edb90edfc09803efb61b066afb
heqins <39974148+heqins@users.noreply.github.com>
2020-06-29 13:28:48 +0800

    Update majority_element.java

--- src/num/majority_element.java
+++ src/num/majority_element.java
@@ -29,3 +29,19 @@ class Solution {
         return nums[n/2];
     }
 }
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int x = 0, votes = 0;
+        for(int num : nums){
+            if(votes == 0) x = num;
+            votes += num == x ? 1 : -1;
+        }
+        return x;
+    }
+}
+
+作者：jyd
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/solution/mian-shi-ti-39-shu-zu-zhong-chu-xian-ci-shu-chao-3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
