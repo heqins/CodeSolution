8812976cfd972486deb2c7ea3ac7785977287f9e
heqins <39974148+heqins@users.noreply.github.com>
2020-07-01 18:18:47 +0800

    Update merge_k_sorted_lists.java

--- src/linked_list/merge_k_sorted_lists.java
+++ src/linked_list/merge_k_sorted_lists.java
@@ -1,70 +1,25 @@
+/**
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+
+**/
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class merge_k_sorted_lists {
-    // O(lg k)
-    // Divide and Conquer
-    public ListNode mergeKLists2(ListNode[] lists) {
-        int k = lists.length;
-        if (k == 0)
-            return null;
-        int interval = 1;
-        while (interval < k) {
-            for (int i = 0; i < (k - interval); i += interval * 2)
-                lists[i] = merge(lists[i], lists[i + interval]);
-            interval *= 2;
-        }
-        return lists[0];
-    }
-
-    // Recursive Merge
-    private ListNode merge(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists2(l1.next, l2);
-            return l1;
-        } else{
-            l2.next = mergeTwoLists2(l1, l2.next);
-            return l2;
-        }
-    }
-
-    // Iterative Merge
-    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-        ListNode head = (l1.val <= l2.val) ? l1 : l2;
-        ListNode last = null;
-        while ((l1 != null) && (l2 != null)) {
-            while ((l1 != null) && (l1.val <= l2.val)) {
-                last = l1;
-                l1 = l1.next;
-            }
-            if (last != null) {
-                last.next = l2;
-                last = null;
-            }
-            if (l1 == null)
-                break;
-            while ((l2 != null) && (l2.val <= l1.val)) {
-                last = l2;
-                l2 = l2.next;
-            }
-            if (last != null) {
-                last.next = l1;
-                last = null;
-            }
-        }
-        return head;
-    }
-
     //PriorityQueue
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> q = new PriorityQueue<ListNode>(new Comparator<ListNode>(){
@@ -112,3 +67,46 @@ public class merge_k_sorted_lists {
         }
     }
 }
+
+
+// 分治法
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        n = len(lists)
+        
+        # basic cases
+        if lenth == 0: return None
+        if lenth == 1: return lists[0]
+        if lenth == 2: return self.mergeTwoLists(lists[0], lists[1])
+        
+        # divide and conqure if not basic cases
+        mid = n // 2
+        return self.mergeTwoLists(self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:n]))
+    
+    
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode(0)
+        c1, c2, c3 = l1, l2, res
+        while c1 or c2:
+            if c1 and c2:
+                if c1.val < c2.val:
+                    c3.next = ListNode(c1.val)
+                    c1 = c1.next
+                else:
+                    c3.next = ListNode(c2.val)
+                    c2 = c2.next
+                c3 = c3.next
+            elif c1:
+                c3.next = c1
+                break
+            else:
+                c3.next = c2
+                break
+            
+        return res.next
