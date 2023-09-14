21fdde98dd02945b19b87dd1425dce3b96f72b27
heqin <heqins@hotmail.com>
2023-09-14 20:57:41 +0800

    feat: 拓扑排序

--- src/dp/最长斐波那契数列.java
+++ src/dp/最长斐波那契数列.java
@@ -1,2 +1,19 @@
-package dp;public class 最长斐波那契数列 {
+package dp;
+
+public class 最长斐波那契数列 {
+
+    /**
+     * 题目：输入一个没有重复数字的单调递增的数组，数组中至少
+     * 有3个数字，请问数组中最长的斐波那契数列的长度是多少？例如，
+     * 如果输入的数组是[1，2，3，4，5，6，7，8]，由于其中最长的斐
+     * 波那契数列是1、2、3、5、8，因此输出是5。
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public static int solve(int[] nums) {
+        return 0;
+    }
 }
